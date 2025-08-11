c38920edf7c1483ee9188299718a84b187dfaadf
Hari Prashanth <hariuk12345@gmail.com>
2025-08-11 11:14:32 +0530

    week 2 folder

---
+++ Hackerrank/Week_2/SalesbyMatch.py
@@ -0,0 +1,38 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    color_count = {}
+    for color in ar:
+        color_count[color] = color_count.get(color, 0) + 1
+    total_pairs = 0
+    
+    for count in color_count.values():
+        total_pairs += count // 2
+    return total_pairs
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
