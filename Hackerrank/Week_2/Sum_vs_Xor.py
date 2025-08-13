1071da8777e283ca254a60c67df90f341e6a18f1
Hari Prashanth <hariuk12345@gmail.com>
2025-08-13 20:34:08 +0530

    Updated Folder

---
+++ Hackerrank/Week_2/Sum_vs_Xor.py
@@ -0,0 +1,31 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sumXor' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def sumXor(n):
+    if n == 0:
+        return 1
+        
+    binary_n = bin(n)
+    zero_count = binary_n.count('0') - 1
+    return 2 **zero_count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = sumXor(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
